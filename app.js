3670cf41ff6ae9cba158d5719a2e80b274e239c0
Mikaël Duguay <mikdug2001@gmail.com>
2026-08-19 17:51:24 +0000

    Modification app

--- app.js
+++ app.js
@@ -51,11 +51,6 @@ app.use(expressLayouts);
 /* Tous les fichiers dans /public */
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(
-    '/uploads',
-    express.static(path.join(__dirname, 'public', 'uploads'))
-);
-
 
 /* =========================
    BODY PARSER
