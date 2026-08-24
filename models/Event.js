af4b765ab5bdd63c945bfb58a824e57b7cd67790
Mikaël Duguay <mikdug2001@gmail.com>
2026-08-24 14:59:32 +0000

    Modification event9

--- models/Event.js
+++ models/Event.js
@@ -1,11 +1,75 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: String,
-    date: { type: Date, required: true, index: true },
-    image: String,
-    location: String
-}, { timestamps: true });
+
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    description: {
+        type: String,
+        default: ''
+    },
+
+    /*
+        Ancienne date principale.
+
+        On la conserve pour assurer la compatibilité
+        avec les événements déjà existants.
+    */
+    date: {
+        type: Date,
+        required: true,
+        index: true
+    },
+
+    /*
+        Plusieurs dates précises.
+
+        Exemple :
+
+        5 décembre
+        6 décembre
+        12 décembre
+        13 décembre
+    */
+    dates: [
+        {
+            type: Date
+        }
+    ],
+
+    /*
+        Début d'un événement qui dure plusieurs jours.
+    */
+    startDate: {
+        type: Date,
+        default: null
+    },
+
+    /*
+        Fin d'un événement qui dure plusieurs jours.
+    */
+    endDate: {
+        type: Date,
+        default: null
+    },
+
+    image: {
+        type: String,
+        default: ''
+    },
+
+    location: {
+        type: String,
+        default: ''
+    }
+
+}, {
+    timestamps: true
+});
+
 
 module.exports = mongoose.model('Event', eventSchema);
