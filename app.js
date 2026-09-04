6132fc841c9bb7786923ccff1fc37eca67467197
Mikaël Duguay <mikdug2001@gmail.com>
2026-09-04 14:58:58 +0000

    Modification final 9

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 
     console.log(
-        `Serveur lancé sur http://localhost:${PORT}`
+        `Serveur démarré sur le port ${PORT}`
     );
 
 });
